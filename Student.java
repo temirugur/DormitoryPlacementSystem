2aa877b830b9b241d8fc1f3d8fecff77fdc0ee64
yigitsoydan <43912713+yigitsoydan@users.noreply.github.com>
2018-12-19 12:57:46 +0300

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,4 +1,23 @@
-
-public class Student {
-
+public abstract class Student {
+	private String studentName;
+	private int age;
+	private String homeCity;
+	
+	public Student (String studentName, int age, String homeCity) {
+		this.studentName = studentName;
+		this.age = age;
+		this.homeCity = homeCity;
+	}
+	
+	public String getStudentName() {
+		return studentName;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public String getHomeCity() {
+		return homeCity;
+	}
 }
